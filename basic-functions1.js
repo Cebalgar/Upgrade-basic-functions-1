77c790c911cc8355ec777bb69d3c6e62b84d6a1b
Celia Ballesteros <cebalgar@gmail.com>
2021-11-05 17:47:58 +0100

    Iteracion#2

--- basic-functions1.js
+++ basic-functions1.js
@@ -13,3 +13,21 @@ function sum(numberOne , numberTwo) {
     console.log(result);
     
     //---------------------------------------------------
+
+    const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(avengers) {
+
+    let longestWord= avengers[0];
+
+    for (let i=0; i<avengers.length; i++){
+        if(avengers[i].length>longestWord.length){
+            longestWord=avengers[i];
+        }
+    }
+  return longestWord
+}
+let result = findLongestWord(avengers);
+
+console.log(result);
+
+//---------------------------------------------------------
