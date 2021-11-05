9482a879c20b8f310943ea37e4eac462462f3d9c
Celia Ballesteros <cebalgar@gmail.com>
2021-11-05 17:47:11 +0100

    Iteracion#1

---
+++ basic-functions1.js
@@ -0,0 +1,15 @@
+function sum(numberOne , numberTwo) {
+    
+    if (numberOne < numberTwo){
+        return numberTwo
+    } else{
+           return numberOne
+    }
+        
+    }
+    
+    let result = sum(37 ,21);
+    
+    console.log(result);
+    
+    //---------------------------------------------------
